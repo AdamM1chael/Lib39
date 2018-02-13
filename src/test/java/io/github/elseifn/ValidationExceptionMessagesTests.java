651aeb3092758b41186895b41c96e8a860e3900f
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2018-02-13 10:10:47 +0800

    Allow creation of seeds from a list of words and allow validation of word list - related to #7 towel

---
+++ src/test/java/io/github/elseifn/ValidationExceptionMessagesTests.java
@@ -0,0 +1,48 @@
+/*
+ *  BIP39 library, a Java implementation of BIP39
+ *  Copyright (C) 2017-2018 Tongjian Cui, elseifn
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Original source: https://github.com/elseifn/Lib39
+ *  You can contact the authors via github issues.
+ */
+
+package io.github.elseifn;
+
+import io.github.elseifn.lib39.Validation.InvalidChecksumException;
+import io.github.elseifn.lib39.Validation.InvalidWordCountException;
+import io.github.elseifn.lib39.Validation.UnexpectedWhiteSpaceException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public final class ValidationExceptionMessagesTests {
+
+    @Test
+    public void InvalidWordCountException_message() throws Exception {
+        assertEquals("Not a correct number of words", new InvalidWordCountException().getMessage());
+    }
+
+    @Test
+    public void InvalidChecksumException_message() throws Exception {
+        assertEquals("Invalid checksum", new InvalidChecksumException().getMessage());
+    }
+
+    @Test
+    public void UnexpectedWhiteSpaceException_message() throws Exception {
+        assertEquals("Unexpected whitespace", new UnexpectedWhiteSpaceException().getMessage());
+    }
+
+}
